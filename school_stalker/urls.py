cc047fcf81008569766b61d6f3ff836548b74c94
gpzim98 <gpzim98@gmail.com>
2017-03-26 16:36:35 +0100

    Improving school app configurations

--- school_stalker/urls.py
+++ school_stalker/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from schools import urls as school_urls
 
 urlpatterns = [
-    url(r'schools/', school_urls,),
     url(r'^admin/', admin.site.urls),
 ]
