cdce5a98cb01cb0c3183a4c6d2e784369dd5324f
gpzim98 <gpzim98@gmail.com>
2017-04-18 16:26:27 +0100

    Item 4 of the email

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from .views import HomeView
+from .views import HomeView, AboutUs
 
 from ..schools.api.view import SchoolViewSet
 router = routers.DefaultRouter()
@@ -10,6 +10,7 @@ router.register(r'schools', SchoolViewSet)
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name="homepage"),
+    url(r'^about-us/$', AboutUs.as_view(), name="about-us"),
     url(r'^api/', include(router.urls)),
 ]
 
--- apps/core/views.py
+++ apps/core/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+
+class AboutUs(TemplateView):
+    template_name = 'aboutus.html'
